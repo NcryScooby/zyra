0a8b5945db9e9e6fb641bfcb77209146c44ec39b
Fabrício <fabricio_c_anelli@hotmail.com>
2025-08-19 23:03:46 -0300

    feat: add banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Banner from "@/components/banner/page";
 import Header from "@/components/header/page";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Banner
+        title="Confira os novos produtos"
+        description="Descubra nossa mais nova coleção hoje mesmo."
+        image="/banner-image.png"
+        action={{
+          text: "Ver coleção completa",
+          href: "/produtos",
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/banner/page.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ArrowRightIcon } from "lucide-react";
+
+interface IBannerProps {
+  title: string;
+  description: string;
+  image: string;
+  action?: {
+    text: string;
+    href: string;
+  };
+}
+
+export default function Banner({
+  title,
+  description,
+  image,
+  action,
+}: IBannerProps) {
+  return (
+    <section className="flex justify-center items-center bg-accent">
+      <div className="flex items-center gap-48 max-w-7xl mx-auto">
+        <div className="flex flex-col gap-12 justify-center">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-bold">{title}</h1>
+            <p className="text-base text-neutral-600">{description}</p>
+          </div>
+          {action && (
+            <Button
+              variant="default"
+              className="bg-primary w-fit"
+              asChild
+              size="lg"
+            >
+              <Link href={action.href}>
+                {action.text}
+                <ArrowRightIcon className="w-4 h-4" />
+              </Link>
+            </Button>
+          )}
+        </div>
+        <Image
+          className="pt-16"
+          src={image}
+          alt="Banner Novos Produtos"
+          width={300}
+          height={300}
+          priority
+        />
+      </div>
+    </section>
+  );
+}
