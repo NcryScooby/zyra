5ce0222fb21a6ba572c1833378f1fcdf4e685076
Fabrício <fabricio_c_anelli@hotmail.com>
2025-08-19 23:24:14 -0300

    feat: add feature card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/banner/page";
+import FeatureCard from "@/components/feature-card/page";
 import Header from "@/components/header/page";
+import { AwardIcon, ShieldCheckIcon, TruckIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
       <Banner
         title="Confira os novos produtos"
@@ -14,6 +16,23 @@ export default function Home() {
           href: "/produtos",
         }}
       />
-    </div>
+      <div className="flex justify-center max-w-7xl mx-auto p-24 gap-24">
+        <FeatureCard
+          icon={<TruckIcon className="w-6 h-6" strokeWidth={1.5} />}
+          title="Frete grátis"
+          description="Frete grátis para compras acima de R$ 100,00."
+        />
+        <FeatureCard
+          icon={<AwardIcon className="w-6 h-6" strokeWidth={1.5} />}
+          title="Garantia de qualidade"
+          description="Todos os produtos são testados e aprovados pela nossa equipe."
+        />
+        <FeatureCard
+          icon={<ShieldCheckIcon className="w-6 h-6" strokeWidth={1.5} />}
+          title="Compra segura"
+          description="Compra segura com pagamento seguro e garantia de qualidade."
+        />
+      </div>
+    </>
   );
 }
---
+++ src/components/feature-card/page.tsx
@@ -0,0 +1,23 @@
+interface IFeatureCardProps {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}
+
+export default function FeatureCard({
+  icon,
+  title,
+  description,
+}: IFeatureCardProps) {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex items-center gap-2 bg-accent rounded-full p-2 w-fit">
+        {icon}
+      </div>
+      <div className="flex flex-col gap-3">
+        <h3 className="text-base font-medium">{title}</h3>
+        <p className="text-sm text-neutral-600 max-w-64">{description}</p>
+      </div>
+    </div>
+  );
+}
